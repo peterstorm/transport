9f5a48ef25539b70a0bcc71277fc9cf735066f60
Peter Storm <peter.storm@peterstorm.io>
2018-12-20 23:18:32 +0100

    Update transport.sh

--- transport.sh
+++ transport.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # transport.sh
 function tp() {
-    # $@ takes all arguments of the shell script and passes it along to `teleport-exe
-    # which is our tool
+    # $@ takes all arguments of the shell script and passes it along to `transport-exe`
     OUTPUT=`transport-exe $@`
-    # return code 2 tells the shell script to cd to whatever `teleport` outputs
+    # return code 2 tells the shell script to `cd` to what `transport` outputs
     if [ $? -eq 2 ]
         then cd "$OUTPUT"
         else echo "$OUTPUT"
